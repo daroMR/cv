e2703e42a84846b640b04ab572a00faba9d2a519
Dario Molina <drqbeat@gmail.com>
2025-06-06 02:20:33 -0600

    Correccion de Linkedin

--- scripts.js
+++ scripts.js
@@ -1,15 +1,14 @@
 // scripts.js
-// Descargar PDF del CV desde la carpeta local PDF
+// Descargar PDF del CV desde GitHub Pages
 document.getElementById('download-pdf').addEventListener('click', function () {
     const link = document.createElement('a');
-    link.href = 'PDF/CV_Hesvin_Dario_Molina_Rios_2025.pdf'; // Ruta relativa al archivo
+    link.href = 'https://daroMR.github.io/cv/PDF/CV_Hesvin_Dario_Molina_Rios_2025.pdf'; // Enlace absoluto a GitHub Pages
     link.download = 'CV_Hesvin_Dario_Molina_Rios_2025.pdf';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
 });
 
-// Botón para ir a LinkedIn
 document.getElementById('linkedin-btn').addEventListener('click', function () {
     window.open('https://linkedin.com/in/drqbeat', '_blank');
 });
