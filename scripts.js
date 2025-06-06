2bbfeb2e401566dd9d5b1fb18137317299047311
Dario Molina <drqbeat@gmail.com>
2025-06-06 02:12:06 -0600

    Correccion de Nombre

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 // Descargar PDF del CV desde la carpeta local PDF
 document.getElementById('download-pdf').addEventListener('click', function () {
     const link = document.createElement('a');
-    link.href = 'PDF/CV_Hesvin_Dario_Molina_Rios_2025.pdf'; // Ruta relativa local
+    link.href = 'PDF/CV_Hesvin_Dario_Molina_Rios_2025.pdf'; // Ruta relativa al archivo
     link.download = 'CV_Hesvin_Dario_Molina_Rios_2025.pdf';
     document.body.appendChild(link);
     link.click();
