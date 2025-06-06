891636e88d2d6008f02443b53f5b019868a1de75
Dario Molina <drqbeat@gmail.com>
2025-06-06 01:54:42 -0600

    v1.0.2

--- scripts.js
+++ scripts.js
@@ -2,8 +2,8 @@
 // Descargar PDF del CV usando html2canvas y jsPDF
 document.getElementById('download-pdf').addEventListener('click', function () {
     const link = document.createElement('a');
-    link.href = 'PDF/CV_Hesvin_Molina_2025.PDF';
-    link.download = 'CV_Hesvin_Molina_2025.PDF';
+    link.href = 'PDF/CV_Hesvin_Dario_Molina_Rios_2025.PDF';
+    link.download = 'CV_Hesvin_Dario_Molina_Rios_2025.PDF';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
